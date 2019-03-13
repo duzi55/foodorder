5a06acf2183c2cb753b227120dea8814d0d2fdb6
duzi55 <1375744701@qq.com>
2019-03-13 21:42:00 +0800

    'we'

---
+++ src/main/java/com/gzq/foodorder/FoodorderApplication.java
@@ -0,0 +1,13 @@
+package com.gzq.foodorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodorderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodorderApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/gzq/foodorder/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.gzq.foodorder.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @RequestMapping("test")
+    public String Test(){
+        return "hello word!";
+    }
+}
