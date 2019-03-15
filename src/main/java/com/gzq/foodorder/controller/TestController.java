c6e9980cf54b7192512d45f7cb3826eabf616c87
duzi55 <1375744701@qq.com>
2019-03-15 10:50:56 +0800

    3-15

--- src/main/java/com/gzq/foodorder/controller/TestController.java
+++ src/main/java/com/gzq/foodorder/controller/TestController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-    @RequestMapping("test")
     public String Test(){
         return "hello word!";
     }
